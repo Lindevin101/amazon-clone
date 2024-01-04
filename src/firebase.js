fcc7598dc9373a6fc8aad88107150af7117f91eb
dlin <lindevin101@gmail.com>
2024-01-04 12:51:17 -0500

    install firebase globally

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDZSzkxPofKV5ImEcyYayvXLXIeK_xRzoU",
+  authDomain: "clone-11e7e.firebaseapp.com",
+  projectId: "clone-11e7e",
+  storageBucket: "clone-11e7e.appspot.com",
+  messagingSenderId: "963075335531",
+  appId: "1:963075335531:web:62396558feb4a65d5bd82e",
+  measurementId: "G-P971JEL9WW",
+};
