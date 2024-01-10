e9b0697c24a4bc63f22716567df0b96ff71b3c95
dlin <lindevin101@gmail.com>
2024-01-10 17:29:54 -0500

    remove buttton is functional

--- src/Checkout.js
+++ src/Checkout.js
@@ -25,7 +25,6 @@ function Checkout() {
               rating={item.rating}
             />
           ))}
-          {/* CheckoutProduct*/}
         </div>
       </div>
 
--- src/CheckoutProduct.js
+++ src/CheckoutProduct.js
@@ -1,10 +1,20 @@
 import React from "react";
 import "./CheckoutProduct.css";
+import { useStateValue } from "./StateProvider";
 
 function CheckoutProduct({ id, image, title, price, rating }) {
+  const [{ Basket }, dispatch] = useStateValue();
+  const removeFromBasket = () => {
+    dispatch({
+      type: "REMOVE_FROM_BASKET",
+      id: id,
+    });
+  };
+
   return (
-    <div className="chcekoutProduct">
+    <div className="checkoutProduct">
       <img className="checkoutProduct__image" src={image} />
+
       <div className="checkoutProduct__info">
         <p className="checkoutProduct__title">{title}</p>
         <p className="checkoutProduct__price">
@@ -14,11 +24,11 @@ function CheckoutProduct({ id, image, title, price, rating }) {
         <div className="checkoutProduct__rating">
           {Array(rating)
             .fill()
-            .map(() => (
+            .map((_, i) => (
               <p>⭐️</p>
             ))}
         </div>
-        <button>Remove From basket</button>
+        <button onClick={removeFromBasket}>Remove From basket</button>
       </div>
     </div>
   );
