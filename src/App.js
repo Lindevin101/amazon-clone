553fa96460412d3bda7ac9ecfacb3c90adaa8b70
dlin <lindevin101@gmail.com>
2024-01-10 21:25:47 -0500

    created login page and add firebase

--- src/App.js
+++ src/App.js
@@ -3,16 +3,37 @@ import Checkout from "./Checkout";
 import Header from "./Header";
 import Home from "./Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./Login";
 
 function App() {
   return (
     //BEM
     <Router>
       <div className="App">
-        <Header />
         <Routes>
-          <Route path="/checkout" element={<Checkout />} />
-          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />}></Route>
+        </Routes>
+        <Routes>
+          <Route
+            path="/checkout"
+            element={
+              <>
+                <Header />
+                <Checkout />
+              </>
+            }
+          />
+        </Routes>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <Header />
+                <Home />
+              </>
+            }
+          />
         </Routes>
       </div>
     </Router>
