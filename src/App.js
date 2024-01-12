b5e37b5b472594fd27f6690bb0e447086aad3fa6
dlin <lindevin101@gmail.com>
2024-01-12 17:15:52 -0500

    signout button brings you back to the home page

--- src/App.js
+++ src/App.js
@@ -4,8 +4,30 @@ import Header from "./Header";
 import Home from "./Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./Login";
+import { useEffect } from "react";
+import { useStateValue } from "./StateProvider";
+import { auth } from "./firebase";
 
 function App() {
+  const [{}, dispatch] = useStateValue();
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      console.log("the user is ", authUser);
+
+      if (authUser) {
+        dispatch({
+          type: "SET_USER",
+          user: authUser,
+        });
+      } else {
+        dispatch({
+          type: "SET_USER",
+          user: null,
+        });
+      }
+    });
+  }, []);
+
   return (
     //BEM
     <Router>
