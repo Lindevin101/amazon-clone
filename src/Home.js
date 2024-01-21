234784ac82bfc553ab5c06452611ff4d84990154
dlin <lindevin101@gmail.com>
2024-01-21 02:50:11 -0500

    product name changes

--- src/Home.js
+++ src/Home.js
@@ -31,7 +31,7 @@ function Home() {
         <div className="home__row">
           <Product
             id="4903850"
-            title="Samsung LC49RG90SSUXEN 49' Curved LED Gaming Monitor"
+            title="FitBit 8 Sensor Smart Lightweight Watch"
             price={199.99}
             image="https://images-na.ssl-images-amazon.com/images/I/71Swqqe7XAL._AC_SX466_.jpg"
             rating={3}
