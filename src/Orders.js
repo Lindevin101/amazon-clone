568693e9f41ce0e2241862176472f5925742330f
dlin <lindevin101@gmail.com>
2024-01-21 01:35:43 -0500

    order history is now visable on orders page

--- src/Orders.js
+++ src/Orders.js
@@ -1,10 +1,48 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Orders.css";
+import { db } from "./firebase";
+import { useStateValue } from "./StateProvider";
+import Order from "./Order";
+import {
+  collection,
+  query,
+  orderBy,
+  onSnapshot,
+  doc,
+} from "firebase/firestore";
 
 function Orders() {
+  const [orders, setOrders] = useState([]);
+  const [{ basket, user }, dispatch] = useStateValue();
+
+  useEffect(() => {
+    if (user) {
+      const ordersRef = collection(db, "users", user?.uid, "orders");
+      const q = query(ordersRef, orderBy("created", "desc"));
+
+      const unsubscribe = onSnapshot(q, (snapshot) => {
+        setOrders(
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            data: doc.data(),
+          }))
+        );
+      });
+
+      return () => unsubscribe(); // Detach listener when the component unmounts
+    } else {
+      setOrders([]);
+    }
+  }, [user]);
+
   return (
     <div className="orders">
       <h1>Your Orders</h1>
+      <div className="orders__order">
+        {orders?.map((order) => (
+          <Order key={order.id} order={order} />
+        ))}
+      </div>
     </div>
   );
 }
